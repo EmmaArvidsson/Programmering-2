0515eaea71e690f86d88769c0aff4cdc71c04706
Johan <johan.hasselblad@gmail.com>
2020-01-24 14:49:24 +0100

    API

---
+++ API/API/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace API
+{
+    class Program
+    {
+        var client = new RestClient("http://adressen");
+        var request = new RestRequest("/", Method.GET);
+        IRestResponse response = client.Execute(request);
+        String content = response.Content;
+        Menu menu = JsonConvert.DeserializeObject<Menu>(content); // Byt ut till er eget huvudobjekt
+
+    }
+}
